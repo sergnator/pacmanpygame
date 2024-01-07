4e453536ee1390e7c35cf5b14fce9c19ece22886
sergnator <86975413+sergnator@users.noreply.github.com>
2024-01-07 20:57:04 +0300

    файл констант

--- BaseClasses.py
+++ BaseClasses.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import pygame
+import Constants
+
 
 # исключения
 class BasePacmanExceptionsGroup(Exception):
@@ -9,20 +11,6 @@ class BasePacmanExceptionsGroup(Exception):
 
 class NameTaken(BasePacmanExceptionsGroup):
     pass
-
-
-# константы
-class Constants:
-    DataBaseOfScore = 'data/databases/score.sqlite'
-    Maps = 'data/maps/'
-    WinResult = 1
-    LoseResult = 0
-    Images = 'data/images/'
-    id_key = 0
-    name_key = 1
-    record_time_key = 2
-    record_map_key = 3
-    Music = 'data/music/'
 
 
 class HelpFunctions:
---
+++ Constants.py
@@ -0,0 +1,12 @@
+DataBaseOfScore = 'data/databases/score.sqlite'
+Maps = 'data/maps/'
+WinResult = 1
+LoseResult = 0
+Images = 'data/images/'
+id_key = 0
+name_key = 1
+record_time_key = 2
+record_map_key = 3
+Music = 'data/music/'
+CellWidth = 25
+CellHeight = 25
